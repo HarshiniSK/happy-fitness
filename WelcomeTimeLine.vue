9e0ee4823e07833124fd2653b05bdb7db59a668f
HarshiniSK <55571090+HarshiniSK@users.noreply.github.com>
2020-06-15 17:55:42 +0530

    Update WelcomeTimeLine.vue

--- WelcomeTimeLine.vue
+++ WelcomeTimeLine.vue
@@ -1,20 +1,25 @@
 <template>
   <v-timeline class="mx-5">
-    <v-timeline-item v-for="n in 3" :key="n" color="red lighten-2" large>
-      <template v-slot:opposite>
-        <span>Tus eu perfecto</span>
-      </template>
-      <v-card class="elevation-2">
-        <v-card-title class="headline">Lorem ipsum</v-card-title>
-        <v-card-text>Lorem ipsum dolor sit amet, no nam oblique veritus. Commune scaevola imperdiet nec ut, sed euismod convenire principes at. Est et nobis iisque percipit, an vim zril disputando voluptatibus, vix an salutandi sententiae.</v-card-text>
+    <v-timeline-item v-for="detail in details" :key="detail.caption" color="success">
+      <v-card class="elevation-2 success lighten-5">
+        <v-card-title class="headline">{{detail.caption}}</v-card-title>
+        <v-card-text>{{detail.content}}</v-card-text>
       </v-card>
     </v-timeline-item>
   </v-timeline>
   </template>
 
+
 <script>
 export default {
-  name: "WelcomeTimeLine"
+  name: "WelcomeTimeLine",
+  data : () =>({
+    details:[
+      {caption:'Fitness Comes with Happiness', content:'You are only one workout away from a good mood' },
+      {caption:'Experienced Trainers and Doctors', content:'100+ Doctors and Trainers for your personal guidence.' },
+      {caption:'Fitness is for everyone, everywhere', content:'Wanna gain weight/ loose weigt/ build muscle/ stay fit....then you are at the right place' },
+    ]
+  }),
 };
 </script>
 
